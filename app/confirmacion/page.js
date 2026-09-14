e0904f2ffef9e5cf0daf3747f0e0bc31e71c67cc
Osvaldo <tu_correo@ejemplo.com>
2026-09-14 05:43:46 -0600

    Agrego página de confirmación en App Router

---
+++ app/confirmacion/page.js
@@ -0,0 +1,8 @@
+export default function Confirmacion() {
+  return (
+    <main style={{textAlign: "center", marginTop: "50px"}}>
+      <h1>Confirmación de suscripción</h1>
+      <p>¡Gracias por suscribirte a nuestro servicio!</p>
+    </main>
+  );
+}
